11862fb7b6de520e23229e8a29cefbb8220439b9
Ngoc Phan <49348139+nphan20181@users.noreply.github.com>
2020-02-18 01:15:53 -0600

    Add files via upload

--- modules/global_plot_functions.py
+++ modules/global_plot_functions.py
@@ -31,81 +31,13 @@ def plot_timeseries(df_group, p_title, rows=2, cols=2):
                 
                 hide_borders(axes[r, c], ['top', 'right'])   # hide top and right borders
                 legend = axes[r, c].legend(loc='best', frameon=False)
-                legend_texts = legend.get_texts()
-                
-                n_loans = df_group[variable].sum()
-                pct = round(n_loans*100/total, 2)
-                legend_texts[0].set_text(variable + ' \n(' + str(n_loans) + ' Loans, ' + str(pct) + '%)')
                 
                 if c < 1:  # show y-label only for plots in the first column
-                    axes[r, c].set_ylabel('Number of Loans')
+                    if r == 0:
+                        axes[r, c].set_ylabel('Number of Loans', fontsize=14)
+                    else:
+                        axes[r, c].set_ylabel('Loan Percentage', fontsize=14)
                 else:
                     axes[r, c].set_ylabel('')
-    plt.suptitle(p_title, fontsize=14)
-    plt.savefig('out/issued_loans_2018')
+    plt.suptitle(p_title, fontsize=16)
     plt.show()
-
-def plot_stacked_bar(df_in, title, ylabel='Issued Loans %', fsize=(11, 6), color=['blue', 'orange', 'yellow', 'red']):
-    fig, ax = plt.subplots(figsize=fsize)
-    
-    # plot stacked bar
-    df_in.plot.bar(stacked=True, color=color, ax=ax)
-    
-    # set cell's text for data table
-    cell_text = []
-    df_index = list(df_in.T.index)
-    for index in df_index:
-        cell_text.append(list(df_in.T.loc[index]))
-    
-    # add a data table at the bottom of the axes
-    data_table = plt.table(cellText=cell_text,
-                      rowLabels=df_index,
-                      rowColours=color,
-                      colLabels=list(df_in.T.columns),
-                      loc='bottom')
-    data_table.scale(1, 3)
-    
-    # format plot
-    hide_borders(ax, ['top', 'right'])   # hide top and right borders
-    ax.legend(bbox_to_anchor=(1, 0.95), frameon=False, title=gv.VARS_DESC.get('loan_status_flag'))
-    ax.set_xlabel('')
-    ax.get_xaxis().set_ticks([])
-    ax.get_xaxis().set_ticklabels([])
-    plt.ylabel(ylabel)
-    plt.title('LendingClub\n' + title, fontsize=14)
-    plt.show()
-
-def frequency_plot(df_in, list_cols, cols=1, w=8, h=6, hue=True, xlabel=False, title='', orientation='x'):
-    rows = int(len(list_cols) / cols)
-    fig, axes = plt.subplots(figsize=(w, h), nrows=rows, ncols=cols)
-    index = -1
-    
-    for r in range(rows):
-        for c in range(cols):
-            if rows > 1 and cols > 1:
-                ax = axes[r, c]
-            elif rows == 1 and cols == 1:
-                ax = axes
-            elif rows > 1 and cols == 1:
-                ax = axes[r]
-            else:
-                ax = axes[c]
-            
-            index += 1
-           # if hue == True:
-            sns.countplot(x='loan_status_flag', data=df_in, hue=list_cols[index], ax=ax)
-            ax.legend(loc='best', title=gv.VARS_DESC.get(list_cols[index]), frameon=False)
-           # else:
-           #     sns.countplot(x=list_cols[index], data=df_in, ax=ax)
-           #     plt.xticks(rotation=90)
-            
-            if xlabel == False:
-                ax.set_xlabel('')
-            ax.set_ylabel('')
-            ax.get_yaxis().set_ticks([])
-            ax.get_yaxis().set_ticklabels([])
-            annotate_plot(ax)
-            hide_borders(ax, ['top', 'right', 'left'])   # hide top and right borders
-    plt.suptitle('LendingClub\n2018 Loan Data\nIssued Loans Frequency Distribution' + title, 
-                 fontsize=14)
-    plt.subplots_adjust(left=None, bottom=None, right=0.9, top=0.9, wspace=0.2, hspace=0.2)
--- modules/global_vars.py
+++ modules/global_vars.py
@@ -1,19 +1,15 @@
-LOAN_STATUS_FLAG = ['Fully Paid', 'Low Risk', 'Medium Risk', 'High Risk']
-LOAN_STATUS = [['Fully Paid'],                                                    # good
-               ['Current'],                                                       # current
-               ['In Grace Period', 'Late (16-30 days)', 'Late (31-120 days)'],    # low risk
-               ['Default', 'Charged Off']]                                        # high risk
+LOAN_STATUS_FLAG = ['Fully Paid', 'Default', 'Fully Paid %', 'Default %']
+LOAN_STATUS = [['Fully Paid'],                           # fully paid
+               ['Default', 'Charged Off']]               # default
 
-dict_loan_flag = {LOAN_STATUS_FLAG[0]: LOAN_STATUS[0],   # good
-                  LOAN_STATUS_FLAG[1]: LOAN_STATUS[1],   # current
-                  LOAN_STATUS_FLAG[2]: LOAN_STATUS[2],   # low risk
-                  LOAN_STATUS_FLAG[3]: LOAN_STATUS[3]}   # high risk
+dict_loan_flag = {LOAN_STATUS_FLAG[0]: LOAN_STATUS[0],   # fully paid
+                  LOAN_STATUS_FLAG[1]: LOAN_STATUS[1]}   # default
 
 STATUS_FLAG_COLORS = { 
                       LOAN_STATUS_FLAG[0]: 'green',      # good
-                      LOAN_STATUS_FLAG[1]: 'blue',       # current
-                      LOAN_STATUS_FLAG[2]: 'orange',     # low risk
-                      LOAN_STATUS_FLAG[3]: 'red'}        # high risk
+                      LOAN_STATUS_FLAG[1]: 'red',        # default
+                      LOAN_STATUS_FLAG[2]: 'green',      # good
+                      LOAN_STATUS_FLAG[3]: 'red'}        # default
 
 VARS_DESC = {'loan_amnt': 'Loan Amount', 'loan_status_flag': 'Loan Status Flag', 
              'hardship_flag': 'Hardship Flag', 'application_type': 'Application Type',
@@ -25,8 +21,8 @@ VARS_DESC = {'loan_amnt': 'Loan Amount', 'loan_status_flag': 'Loan Status Flag',
              'int_rate': 'Interest Rate', 'dti': 'Debt Ratio',
              'yrs_since_earliest_cr_line': 'Years since Earliest Credit Line', 
              'installment': 'Installment', 'total_pymnt': 'Total Payment', 
-             'total_rec_int': 'Interest Received to Date', 'loan_return': 'Loan Return', 
+             'total_rec_int': 'Interest Received to Date', 
              'total_rec_prncp': 'Principal Received to Date',
              'total_acc': 'Total Credit Lines', 'total_bal_il': 'Total Installment Acct Balance',
-             'out_prncp': 'Remaining Outstanding Principal'
+             'out_prncp': 'Remaining Outstanding Principal', 'revol_util': 'revol_util'
              }
