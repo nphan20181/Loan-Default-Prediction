8abc4df00e6c49cfd6e8c87bbfb4b4ce5edf4ff4
Ngoc Phan <49348139+nphan20181@users.noreply.github.com>
2020-02-29 20:24:52 -0600

    Add files via upload

--- modules/global_plot_functions.py
+++ modules/global_plot_functions.py
@@ -12,32 +12,3 @@ def annotate_plot(ax):
         ax.annotate(format(p.get_height(), '.0f'), 
                     (p.get_x() + p.get_width() / 2., p.get_height()), ha = 'center', va = 'center', 
                     xytext = (0, 10), textcoords = 'offset points')
-
-def plot_timeseries(df_group, p_title, rows=2, cols=2):
-    fig, axes = plt.subplots(figsize=(20, 10), nrows=rows, ncols=cols)
-    list_of_cols = list(df_group.columns)
-    total = df_group.sum(axis=0).sum()    # total number of loans in the dataset
-    
-    X = list(df_group.index)
-    for r in range(rows):
-        for c in range(cols):
-            if len(list_of_cols) > 0:
-                variable = list_of_cols.pop()
-                temp_df = df_group[variable]
-                color = gv.STATUS_FLAG_COLORS.get(variable)
-                
-                axes[r, c].plot(list(temp_df), '-', c=color, label=variable)
-                axes[r, c].scatter(X, temp_df, c=color, label=None)
-                
-                hide_borders(axes[r, c], ['top', 'right'])   # hide top and right borders
-                legend = axes[r, c].legend(loc='best', frameon=False)
-                
-                if c < 1:  # show y-label only for plots in the first column
-                    if r == 0:
-                        axes[r, c].set_ylabel('Number of Loans', fontsize=14)
-                    else:
-                        axes[r, c].set_ylabel('Loan Percentage', fontsize=14)
-                else:
-                    axes[r, c].set_ylabel('')
-    plt.suptitle(p_title, fontsize=16)
-    plt.show()
--- modules/global_vars.py
+++ modules/global_vars.py
@@ -1,4 +1,5 @@
-LOAN_STATUS_FLAG = ['Fully Paid', 'Default', 'Fully Paid %', 'Default %']
+LOAN_STATUS_FLAG = ['Fully Paid', 'Default']
+LOAN_STATUS_PCT = ['Fully Paid %', 'Default %']
 LOAN_STATUS = [['Fully Paid'],                           # fully paid
                ['Default', 'Charged Off']]               # default
 
@@ -7,22 +8,18 @@ dict_loan_flag = {LOAN_STATUS_FLAG[0]: LOAN_STATUS[0],   # fully paid
 
 STATUS_FLAG_COLORS = { 
                       LOAN_STATUS_FLAG[0]: 'green',      # good
-                      LOAN_STATUS_FLAG[1]: 'red',        # default
-                      LOAN_STATUS_FLAG[2]: 'green',      # good
-                      LOAN_STATUS_FLAG[3]: 'red'}        # default
+                      LOAN_STATUS_FLAG[1]: 'red'}        # default
 
 VARS_DESC = {'loan_amnt': 'Loan Amount', 'loan_status_flag': 'Loan Status Flag', 
              'hardship_flag': 'Hardship Flag', 'application_type': 'Application Type',
              'debt_settlement_flag': 'Debt Settlement Flag', 'term': 'Loan Term',
-             'pymnt_plan': 'Payment Plan', 'initial_list_status': 'Initial List Status',
+             'pymnt_plan': 'Payment Plan', 
              'emp_length': 'Employment Length', 'title':'Loan Purpose Desc',
-             'grade': 'Grade', 'verification_status': 'Verification Status', 
+             'grade': 'Grade', 
              'home_ownership': 'Home Ownership',
              'int_rate': 'Interest Rate', 'dti': 'Debt Ratio',
              'yrs_since_earliest_cr_line': 'Years since Earliest Credit Line', 
              'installment': 'Installment', 'total_pymnt': 'Total Payment', 
-             'total_rec_int': 'Interest Received to Date', 
-             'total_rec_prncp': 'Principal Received to Date',
              'total_acc': 'Total Credit Lines', 'total_bal_il': 'Total Installment Acct Balance',
              'out_prncp': 'Remaining Outstanding Principal', 'revol_util': 'revol_util'
              }
